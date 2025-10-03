45b288e9020fdce947a9963baac7bdb0e64cbcc9
Jamugisa-Peter-Paul <jamugisapeter21@gmail.com>
2025-10-03 09:14:27 +0300

    initial commit

---
+++ file.py
@@ -0,0 +1,15 @@
+#print("Hello Python")
+#print("#" * 10)
+#print ('hello')
+#x = 10
+#y = 20
+#unitprice = 3.45
+#an expression
+#x + y * 2
+
+#Variables
+student_count = 1000 #interger
+rating = 4.99 #float
+is_published = True #boolean
+course_name = "Python Programming" #string
+hash(course_name)#hash value of the string
