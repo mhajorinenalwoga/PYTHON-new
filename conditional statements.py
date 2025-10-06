1aa6cf25d9d039613e837ba15fe14b0f6c8cc78b
mhajorinenalwoga <majorinenalwoga5@.com>
2025-10-06 17:35:17 +0300

    conditional statements

--- conditional statements.py
+++ conditional statements.py
@@ -1,12 +1,18 @@
-temperature = 40
-if temperature > 30:
-    print("It's a hot day")
-    print("Drink plenty of water")
-# if temperature < 20:
-#   pass
-# else:
-#     print("It's not a hot day")
-# pass
-elif temperature < 20:
-    print("It's a nice day")
-    print("Enjoy your day") 
+# temperature = 40
+# if temperature > 30:
+#     print("It's a hot day")
+#     print("Drink plenty of water")
+# # if temperature < 20:
+# #   pass
+# # else:
+# #     print("It's not a hot day")
+# # pass
+# elif temperature < 20:
+#     print("It's a nice day")
+#     print("Enjoy your day") 
+
+age =22
+if age >= 18:
+    print("You are a minor")
+else:
+    print("You are an adult")
