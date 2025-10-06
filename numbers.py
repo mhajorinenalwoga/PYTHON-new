c4bff988034c6d4419a09d97f5e2c777d45a6a53
mhajorinenalwoga <majorinenalwoga5@.com>
2025-10-06 15:33:07 +0300

    Numbers and math functions in Python

---
+++ numbers.py
@@ -0,0 +1,31 @@
+x = 1 #intergers
+x = 1.1 #floats
+x = 1 + 2j # a + bi (complex numbers)
+
+print(10 / 3) #division always returns a float
+print(10 // 3) #floor division returns an integer   
+print(10 % 3) #modulus returns the remainder
+print(10 ** 3) #exponentiation
+print (10 +3) #addition
+print(10 - 3) #subtraction
+print(10 * 3) #multiplication
+print(round(2.9)) #rounds to the nearest integer
+print(abs(-2.9)) #absolute value
+
+
+x =10
+x = x + 3
+x += 3 #shorthand for x = x + 3
+x -= 3 #shorthand for x = x - 3
+
+import math
+#FUNCTIONS TO USE WITH NUMBERS
+print(round(2.9)) #rounds to the nearest integer
+print(abs(-2.9)) #absolute value
+print(pow(2, 3)) #2 to the power of 3
+
+math.ceil(2.1) #rounds up to the nearest integer
+print(math.ceil(2.1)) #rounds down to the nearest integer
+print(math.floor(2.9)) #rounds down to the nearest integer
+print(math.sqrt(16)) #square root
+
